7f1d0fe7e37117a61e93f521d84f6ba0c53aa9d5
Lucila Kim <lucila.kim.9@gmail.com>
2022-09-06 23:25:14 -0300

    add the request that i made

--- 2022-09-01-node-challenge/npm-proyect/server.js
+++ 2022-09-01-node-challenge/npm-proyect/server.js
@@ -9,3 +9,17 @@ server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
+
+
+
+/* 
+
+Peticiones al servidor
+
+0. Obtener todos los usuarios.
+1. Obtener un user particular
+2. Crear un nuevo usuario que no exista
+3. Modificar un usuario
+4. Eliminar un usuario
+
+*/
